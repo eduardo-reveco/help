d27b84a06843d3ca35322a078ec6b60915329235
john tremont <piloto2810@gmail.com>
2021-09-03 23:44:08 -0400

    creadas acciones para services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,8 @@
 class ServicesController < ApplicationController
+  before_action :service_params, only: %i[create]
+  before_action :set_service, only: %i[show edit update destroy]
+  before_action :set_user, only: %i[new create]
+
   def index
     @services = Service.all
   end
@@ -8,6 +12,26 @@ class ServicesController < ApplicationController
   end
 
   def create
-    @service = Service.new(params[:id])
+    @service = Service.new(service_params)
+    @service.user = @user
+    @service.save
+    redirect_to service_path(@service)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:name, :description, :price)
   end
 end
