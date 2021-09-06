e606744df64fad0b226c14d889e4652c59e06e73
ChristianFlores <cfloreshidalgo@gmail.com>
2021-09-06 17:52:07 -0300

    upgrade 2.7.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_09_04_010637) do
-=======
-ActiveRecord::Schema.define(version: 2021_09_04_004808) do
->>>>>>> 87640ad00228c577bf4afb34df2a8e3a25117e06
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "services", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -30,8 +25,6 @@ ActiveRecord::Schema.define(version: 2021_09_04_004808) do
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
-=======
->>>>>>> 87640ad00228c577bf4afb34df2a8e3a25117e06
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,12 +34,10 @@ ActiveRecord::Schema.define(version: 2021_09_04_004808) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   add_foreign_key "services", "users"
-=======
->>>>>>> 87640ad00228c577bf4afb34df2a8e3a25117e06
 end
