4e2287794d8f8339bce24c01c2929965f1f9c578
john tremont <piloto2810@gmail.com>
2021-09-06 17:15:34 -0300

    agregado pundit a servicios, queda pendiente rol de admin y acceso a links

--- app/policies/service_policy.rb
+++ app/policies/service_policy.rb
@@ -3,9 +3,21 @@ class ServicePolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+  end
 
-    def create?
-      return true
-    end
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
   end
 end
