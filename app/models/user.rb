28f124927b3cc9ac89e4f8464c644aad61ed0303
john tremont <piloto2810@gmail.com>
2021-09-10 13:28:58 -0300

    agregadas categorias a modelo user 'facilitador-cliente'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  ROLES = %w[facilitador cliente]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :services
   has_many :bookings
+
+  validates :rol, inclusion: { in: ROLES, allow_nil: false, message: "Escoge entre: facilitador o cliente" }
 end
