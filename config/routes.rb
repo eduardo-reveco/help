5693ca198c1639b8ec08b63d4c539ccb21549797
john tremont <piloto2810@gmail.com>
2021-09-13 18:51:01 -0300

    creado controlador para reviews y vista de new

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   devise_for :users
   root to: 'pages#home'
   get '/profile', to: 'services#profile'
   
   resources :services do
     resources :bookings, only: %i[create]
-    resources :reviews, only: %i[create]
+    resources :reviews, only: %i[new create]
   end
   resources :bookings, only: %i[index show destroy]  
 end
